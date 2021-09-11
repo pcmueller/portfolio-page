9af5ee06449491e6379936159ea4f1ca45528914
pcmueller <pmuellerleile@gmail.com>
2021-09-11 16:45:51 -0600

    Structure and style About page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import { Route, NavLink, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import About from '../About/About';
 
 const App = () => {
   return (
     <Router>
       <div className="app">
-        <header className='app-header'>
-            <div></div>
-            <div className='app-title-container'>
-              <NavLink to='/' className='home-link'>
-                <h1 className='app-title'>Pete's Wacky Code Party</h1>
-              </NavLink>
-            </div>
-          </header>
+        <Route exact path='/'>
+          <About />
+        </Route>
       </div>
-      <Route exact path='/'>
-      </Route>
     </Router>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav className='nav-bar'>
+      <div>
+        NAV BAR
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar;
