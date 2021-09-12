5d86f0b5fb3fc4cc8e0e47ae036f176ee0deccf2
pcmueller <pmuellerleile@gmail.com>
2021-09-11 18:38:08 -0600

    Fix menu and overlay locations

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,18 +8,16 @@ const NavBar = () => {
   }
 
   return (
-    <nav className='nav-bar'>
-      <Menu 
-        pageWrapId={'page-wrap'} 
-        outerContainerId={'outer-container'}
-        width={ '30%' }
-      >
-        <a id='home' className='menu-item' href='/'>Home</a>
-        <a id='about' className='menu-item' href='/about'>About</a>
-        <a id='contact' className='menu-item' href='/contact'>Contact</a>
-        <a onClick={(e) => showSettings(e)} className='menu-item--small' href='/'>Settings</a>
-      </Menu>
-    </nav>
+    <Menu 
+      pageWrapId={'page-wrap'} 
+      outerContainerId={'outer-container'}
+      className={'burger-menu'}
+    >
+      <a id='home' className='menu-item' href='/'>Home</a>
+      <a id='about' className='menu-item' href='/about'>About</a>
+      <a id='contact' className='menu-item' href='/contact'>Contact</a>
+      <a onClick={(e) => showSettings(e)} className='menu-item--small' href='/'>Settings</a>
+    </Menu>
   )
 }
 
