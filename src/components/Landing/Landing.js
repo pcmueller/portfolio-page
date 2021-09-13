33ca49725bbb320fbe7c6fad94819c508d04838f
pcmueller <pmuellerleile@gmail.com>
2021-09-13 17:01:03 -0600

    Add VHS images and set as Landing background

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Landing = () => {
   return (
-    <main className='landing-page'>
+    <main className='landing-page' style={{"background":"url(images/VHS_Noise_Pack/2.png)"}}>
       LANDING PAGE
     </main>
   )
