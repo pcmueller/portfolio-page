522fa2168b28f31a21148a7071294ab045cf66f5
pcmueller <pmuellerleile@gmail.com>
2021-09-16 18:25:32 -0600

    Remove flicker effect

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -4,9 +4,9 @@ const Landing = () => {
 
   return (
     <main 
-      className='landing-page js-demo vhs-highlight-pop' 
+      className='landing-page' 
       style={{"background":"url(images/VHS_Noise_Pack/2.png)", "width":"100vw", "height":"100vh", "backgroundSize":"cover"}}>
-        <h1>hello world.</h1>
+        <h1 className='vhs-fade vhs-duration-6 vhs-infinite vhs-alternate js-demo'>hello world.</h1>
     </main>
   )
 }
