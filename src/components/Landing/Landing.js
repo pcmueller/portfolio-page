bcf1a9fb252ed6f63b33590bf5fd32872e131356
pcmueller <pmuellerleile@gmail.com>
2021-09-18 16:56:15 -0600

    Install GSAP

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import vhsEffect from '../../utilities/effects';
 
 const Landing = () => {
+
+  vhsEffect();
 
   return (
     <main 
       className='landing-page' 
       style={{"background":"url(images/VHS_Noise_Pack/2.png)", "width":"100vw", "height":"100vh", "backgroundSize":"cover"}}>
-        <h1 className='vhs-fade vhs-duration-6 vhs-infinite vhs-alternate js-demo'>hello world.</h1>
+        <h1 id='text'>hello world.</h1>
     </main>
   )
 }
