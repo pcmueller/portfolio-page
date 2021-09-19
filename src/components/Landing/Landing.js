b2ee602e87bf26ad37a81321e7cead0efc0575f2
pcmueller <pmuellerleile@gmail.com>
2021-09-19 16:31:20 -0600

    Add glitch effect

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -9,7 +9,12 @@ const Landing = () => {
     <main 
       className='landing-page' 
       style={{"background":"url(images/VHS_Noise_Pack/2.png)", "width":"100vw", "height":"100vh", "backgroundSize":"cover"}}>
-        <h1 id='text'>hello world.</h1>
+        <div className="container">
+          <p className="subtitle">WELCOME TO</p>
+          <div className="glitch" data-text="PMUELLER.net">PMUELLER.net</div>
+          <div className="glow">PMUELLER.net.</div>
+        </div>
+        <div className="scanlines"></div>
     </main>
   )
 }
