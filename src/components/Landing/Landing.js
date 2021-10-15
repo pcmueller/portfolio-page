e8629bd5e2d23fa23839ff5e89bf620c7cb78a56
pcmueller <pmuellerleile@gmail.com>
2021-10-15 16:18:24 -0600

    Rebuild Landing Page FX

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,20 +1,46 @@
 import React from 'react';
-import vhsEffect from '../../utilities/effects';
 
 const Landing = () => {
 
-  vhsEffect();
+  drawLines();
+
+  function getHeight(){
+    return window.innerHeight
+  || document.documentElement.clientHeight
+  || document.body.clientHeight;
+  }
+
+  function drawLines(){
+    const lines = document.getElementsByClassName('line');
+    if(lines.length) {
+      for (let i = 0; i < lines.length; i++) {
+          document.body.removeChild(lines[i]);
+      }
+    }
+    
+    for(let i = 0; i < getHeight()/10; i++){
+      const line = document.createElement("div");  
+      line.className = `line line-${i}`;
+      line.style.top = `${i * 10}px`;
+      const time = Math.random() * 5;
+      line.style.animation = `lines ${time}s infinite`;
+      document.body.appendChild(line) ;
+    }
+  }
+
+  window.onresize = function(event) {
+    drawLines();
+  };
 
   return (
     <main 
       className='landing-page' 
-      style={{"background":"url(images/VHS_Noise_Pack/2.png)", "width":"100vw", "height":"100vh", "backgroundSize":"cover"}}>
-        <div className="container">
-          <p className="subtitle">WELCOME TO</p>
-          <div className="glitch" data-text="PMUELLER.net">PMUELLER.net</div>
-          <div className="glow">PMUELLER.net.</div>
+      style={{"background":"url(images/tube-tv.png)", "width":"100vw", "height":"100vh", "backgroundSize":"cover"}}
+      >
+        <div className='container'>
+          <p className='glitch'>WELCOME TO</p>
+          <p className='glitch'>PETE CODES</p>
         </div>
-        <div className="scanlines"></div>
     </main>
   )
 }
