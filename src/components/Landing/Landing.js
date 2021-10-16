c5b04d88eeae3f7dee97b7873574b15ef2dfbb47
pcmueller <pmuellerleile@gmail.com>
2021-10-15 18:36:33 -0600

    Remove static animation

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,44 +1,10 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 
 const Landing = () => {
 
-  const containerRef = useRef();
-  const { current } = containerRef;
-
-  useEffect(drawLines, [current]);
-
-  function getHeight(screen){
-    return screen.innerHeight
-      || screen.clientHeight;
-  }
-
-  function drawLines() {
-    let screen = document.querySelector('.screen');
-
-    const lines = document.getElementsByClassName('line');
-    if (lines.length) {
-      for (let i = 0; i < lines.length; i++) {
-          screen.removeChild(lines[i]);
-      }
-    }
-    
-    for (let i = 0; i < getHeight(screen)/10; i++){
-      const line = document.createElement("div");
-      line.className = `line line-${i}`;
-      line.style.top = `${i * 10}px`;
-      const time = Math.random() * 5;
-      line.style.animation = `lines ${time}s infinite`;
-      screen.appendChild(line) ;
-    }
-  }
-
-  window.onresize = function(event) {
-    drawLines();
-  };
-
   return (
     <main className='landing-page'>
-        <div ref={containerRef} className='screen'>
+        <div className='screen'>
           <p className='glitch'>WELCOME TO</p>
           <p className='glitch'>PETE CODES</p>
         </div>
