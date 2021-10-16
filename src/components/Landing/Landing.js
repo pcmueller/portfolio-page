a22d90fee3a6ac22283f638bb2ee85b684c98658
pcmueller <pmuellerleile@gmail.com>
2021-10-15 19:10:16 -0600

    Replace static background and style animation

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,6 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const Landing = () => {
+
+  const containerRef = useRef();
+  const { current } = containerRef;
+
+  useEffect(drawLines, [current]);
+
+  function getHeight(screen){
+    return screen.innerHeight
+      || screen.clientHeight;
+  }
+
+  function drawLines() {
+    let screen = document.querySelector('.landing-page');
+
+    const lines = document.getElementsByClassName('line');
+    if (lines.length) {
+      for (let i = 0; i < lines.length; i++) {
+          screen.removeChild(lines[i]);
+      }
+    }
+    
+    for (let i = 0; i < getHeight(screen)/10; i++){
+      const line = document.createElement("div");
+      line.className = `line line-${i}`;
+      line.style.top = `${i * 10}px`;
+      const time = Math.random() * 5;
+      line.style.animation = `lines ${time}s infinite`;
+      screen.appendChild(line) ;
+    }
+  }
+
+  window.onresize = function(event) {
+    drawLines();
+  };
 
   return (
     <main className='landing-page'>
