bae562d20f25a83d180343d01eb41aeeb021f04e
pcmueller <pmuellerleile@gmail.com>
2021-10-18 18:29:25 -0600

    Finish building GitHub components

--- src/components/GitHubCards/GitHubCards.js
+++ src/components/GitHubCards/GitHubCards.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import Axios from "axios";
 import baseURL from "../../utilities/github.js";
-// import RepoCard from "./RepoCard";
+import RepoCard from "../RepoCard/RepoCard";
 import { Grid } from "@material-ui/core";
 
-class Cards extends Component {
+class GitHubCards extends Component {
   Title = [];
   state = {
     repo: [],
@@ -15,14 +15,9 @@ class Cards extends Component {
     const api_key = process.env.REACT_APP_GITHUB_API_KEY;
 
     let repo = [
-      baseURL(`covid-19-tracker`),
-      baseURL(`material-ui-medium-blog`),
-      baseURL(`github-readme-medium-card`),
-      baseURL(`react-youtube-search-clone`),
-      baseURL(`tamil-song-corpus`),
-      baseURL(`Nozama_Warriors`),
-      baseURL(`Nursery_Management`),
-      baseURL(`React-Medium-Blog`),
+      baseURL(`hookshot`),
+      baseURL(`the-wormhole`),
+      baseURL(`cyclorama`),
     ];
 
     await Axios.get("https://github-lang-deploy.herokuapp.com/").then(
@@ -51,14 +46,12 @@ class Cards extends Component {
         : 0
     );
     return (
-      <>
-      </>
-      // <Grid container spacing={1}>
-      //   {repo.map((data, i) => (
-      //     <RepoCard repo={data} key={i} language={language} />
-      //   ))}
-      // </Grid>
+      <Grid container spacing={1}>
+        {repo.map((data, i) => (
+          <RepoCard repo={data} key={i} language={language} />
+        ))}
+      </Grid>
     );
   }
 }
-export default Cards;
+export default GitHubCards;
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import NavMenu from '../NavMenu/NavMenu';
-import Cards from '../Cards/Cards';
+import GitHubCards from '../GitHubCards/GitHubCards';
 
 const Portfolio = () => {
-
 
   return (
     <main className='portfolio-page' id='outer-container'>
@@ -14,7 +13,7 @@ const Portfolio = () => {
       <section className='portfolio-container' id='page-wrap'>
         <h1 className='portfolio-title'>PORTFOLIO</h1>
         <article className='portfolio-section'>
-          <Cards></Cards>
+          <GitHubCards />
         </article>
       </section>
     </main>
